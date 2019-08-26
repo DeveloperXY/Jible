f41fff52a051cbf7b17273de5ab3132fe1d92dc6
Mohammed Aouf ZOUAG <m.a.zouag@gmail.com>
2019-08-26 12:16:38 +0100

    Finish the 1st draft of the landing page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,6 +4,9 @@ import "./heroSection.css";
 import homeIcon from "../images/home.svg";
 import rightArrowIcon from "../images/arrowRight.svg";
 import helmetIcon from "../images/helmet.svg";
+import appImage from "../images/app.png";
+import playStoreImage from "../images/google-play-badge.svg";
+import appStoreImage from "../images/download-on-the-app-store-apple.svg";
 
 const HeroSection = props => {
   return (
@@ -26,13 +29,62 @@ const HeroSection = props => {
           <div className="consumer-btn">
             <img src={homeIcon} class="left-icon" alt="Home" />
             <span className="signup-consumer-text">Signup as a Consumer</span>
-            <img src={homeIcon} class="right-icon" alt="Go" />
+            <img src={rightArrowIcon} class="right-icon" alt="Go" />
           </div>
           <div className="rider-btn">
             <img src={helmetIcon} class="left-icon" alt="Home" />
             <span className="signup-rider-text">Signup as a Rider</span>
-            <img src={helmetIcon} class="right-icon" alt="Go" />
+            <img src={rightArrowIcon} class="right-icon" alt="Go" />
           </div>
+        </div>
+      </div>
+      <div className="how-it-works-header">
+        <p>How it Works</p>
+      </div>
+      <div className="how-it-works-content">
+        <div className="how-it-works-part1">
+          <p className="sub-header">REQUEST YOUR SKHERA</p>
+          <p className="sub-content">Choosing a quality cookware set</p>
+        </div>
+        <div className="how-it-works-part2">
+          <p className="sub-header">EASY ORDERING</p>
+          <p className="sub-content">Serve eggs anytime</p>
+        </div>
+        <div className="how-it-works-part3">
+          <p className="sub-header">REAL-TIME TRACKING</p>
+          <p className="sub-content">
+            Microwave cooking is the next wave of the future
+          </p>
+        </div>
+      </div>
+      <div className="apps-section">
+        <img src={appImage} className="app-image" alt="Mobile app" />
+        <div className="apps-right-section">
+          <div className="apps-header">
+            <p>Track your deliveries with the Jible app</p>
+          </div>
+          <div className="store-logos">
+            <img
+              src={playStoreImage}
+              className="store-img play-store"
+              alt="Play Store"
+            />
+            <img src={appStoreImage} className="store-img" alt="App Store" />
+          </div>
+        </div>
+      </div>
+      <div className="footer-content">
+        <span className="jible-footer-header">Jible</span>
+        <div className="footer-items">
+          <a href="#" className="about-item footer-link">
+            About
+          </a>
+          <a href="#" className="terms-item footer-link">
+            Terms
+          </a>
+          <a href="#" className="policy-item footer-link">
+            Privacy policy
+          </a>
         </div>
       </div>
     </div>
