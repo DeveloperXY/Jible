dda72c623f261acf022551b794ede7cb5dd6bf9c
Mohammed Aouf ZOUAG <m.a.zouag@gmail.com>
2019-09-03 15:55:50 +0100

    Create the RequestSkheraDetails & RequestSkheraMap components

--- src/components/request/RequestSkhera.js
+++ src/components/request/RequestSkhera.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./requestSkhera.css";
+import RequestSkheraDetails from "./details/RequestSkheraDetails";
+import RequestSkheraMap from "./map/RequestSkheraMap";
 
 function RequestSkhera(props) {
   return (
     <>
       <h1 className="request-skhera-header">Request a Skhera</h1>
+      <div className="request-content">
+        <RequestSkheraDetails />
+        <RequestSkheraMap />
+      </div>
     </>
   );
 }
---
+++ src/components/request/map/RequestSkheraMap.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./requestSkheraMap.css";
+
+function RequestSkheraMap(props) {
+  return <></>;
+}
+
+export default RequestSkheraMap;
