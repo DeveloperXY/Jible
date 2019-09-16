cc5d9d12f1083e588be1e5ccd389e4e515aac673
Mohammed Aouf ZOUAG <m.a.zouag@gmail.com>
2019-09-16 11:15:19 +0100

    Make a mobile version of the landing page's UI

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -95,35 +95,40 @@ function HomePage({ history, actions }) {
 
   return (
     <div className="hero">
-      <div className="hero-content">
-        <div className="header-content">
-          <span className="jible-header">Jible</span>
-          <div className="buttons-container">
-            <input type="button" className="green-btn" value="Signup" />
-            <input
-              type="button"
-              className="white-btn"
-              value="Login"
-              onClick={openLoginDialog}
-            />
+      <div className="hero-content-wrapper">
+        <div className="hero-content">
+          <div className="header-content">
+            <span className="jible-header">Jible</span>
+            <div className="buttons-container">
+              <input type="button" className="green-btn" value="Signup" />
+              <input
+                type="button"
+                className="white-btn"
+                value="Login"
+                onClick={openLoginDialog}
+              />
+            </div>
           </div>
-        </div>
-        <div className="content-description">
-          <p>
-            An on demand service that picks-up anything you requested through
-            the app and delivers it to your door within one hour.
-          </p>
-        </div>
-        <div className="signup-section">
-          <div className="consumer-btn" onClick={() => openSignupDialog()}>
-            <img src={homeIcon} className="left-icon" alt="Home" />
-            <span className="signup-consumer-text">Signup as a Consumer</span>
-            <img src={rightArrowIcon} className="right-icon" alt="Go" />
+          <div className="content-description">
+            <p>
+              An on demand service that picks-up anything you requested through
+              the app and delivers it to your door within one hour.
+            </p>
           </div>
-          <div className="rider-btn" onClick={() => openSignupDialog("rider")}>
-            <img src={helmetIcon} className="left-icon" alt="Home" />
-            <span className="signup-rider-text">Signup as a Rider</span>
-            <img src={rightArrowIcon} className="right-icon" alt="Go" />
+          <div className="signup-section">
+            <div className="consumer-btn" onClick={() => openSignupDialog()}>
+              <img src={homeIcon} className="left-icon" alt="Home" />
+              <span className="signup-consumer-text">Signup as a Consumer</span>
+              <img src={rightArrowIcon} className="right-icon" alt="Go" />
+            </div>
+            <div
+              className="rider-btn"
+              onClick={() => openSignupDialog("rider")}
+            >
+              <img src={helmetIcon} className="left-icon" alt="Home" />
+              <span className="signup-rider-text">Signup as a Rider</span>
+              <img src={rightArrowIcon} className="right-icon" alt="Go" />
+            </div>
           </div>
         </div>
       </div>
@@ -131,26 +136,35 @@ function HomePage({ history, actions }) {
         <p>How it Works</p>
       </div>
       <div className="how-it-works-content">
-        <div className="how-it-works-part1">
-          <p className="sub-header">REQUEST YOUR SKHERA</p>
-          <p className="sub-content">Choosing a quality cookware set</p>
+        <div className="how-it-works-part1-wrapper part-wrapper">
+          <div className="how-it-works-part1 how-it-works-part">
+            <p className="sub-header">REQUEST YOUR SKHERA</p>
+            <p className="sub-content">Choosing a quality cookware set</p>
+          </div>
+          <div className="how-it-works-part-empty how-it-works-part"></div>
         </div>
-        <div className="how-it-works-part2">
-          <p className="sub-header">EASY ORDERING</p>
-          <p className="sub-content">Serve eggs anytime</p>
+        <div className="how-it-works-part2-wrapper part-wrapper">
+          <div className="how-it-works-part-empty how-it-works-part"></div>
+          <div className="how-it-works-part2 how-it-works-part">
+            <p className="sub-header">EASY ORDERING</p>
+            <p className="sub-content">Serve eggs anytime</p>
+          </div>
         </div>
-        <div className="how-it-works-part3">
-          <p className="sub-header">REAL-TIME TRACKING</p>
-          <p className="sub-content">
-            Microwave cooking is the next wave of the future
-          </p>
+        <div className="how-it-works-part3-wrapper part-wrapper">
+          <div className="how-it-works-part-empty how-it-works-part"></div>
+          <div className="how-it-works-part3 how-it-works-part">
+            <p className="sub-header">REAL-TIME TRACKING</p>
+            <p className="sub-content">
+              Microwave cooking is the next wave of the future
+            </p>
+          </div>
         </div>
       </div>
       <div className="apps-section">
         <img src={appImage} className="app-image" alt="Mobile app" />
         <div className="apps-right-section">
           <div className="apps-header">
-            <p>Track your deliveries with the Jible app</p>
+            Track your deliveries with the Jible app
           </div>
           <div className="store-logos">
             <img
