f650df40cc063e5c9da54a682d78220bc37e85f2
Mohammed Aouf ZOUAG <m.a.zouag@gmail.com>
2019-09-17 15:15:29 +0100

    Change the background color of the drawer menu items upon selection

--- src/components/rider/drawer/DrawerLayout.js
+++ src/components/rider/drawer/DrawerLayout.js
@@ -5,6 +5,38 @@ import ToggleButton from "react-toggle-button";
 
 export const DrawerLayout = props => {
   const [availability, setAvailability] = useState(false);
+  const [isSkheratTodoSelected, setIsSkheratTodoSelected] = useState(false);
+  const [isMyProfileSelected, setIsMyProfileSelected] = useState(true);
+  const [isStatisticsSelected, setIsStatisticsSelected] = useState(false);
+  const [isFaqSelected, setIsFaqSelected] = useState(false);
+
+  function goToSkherasTodo() {
+    setIsSkheratTodoSelected(true);
+    setIsMyProfileSelected(false);
+    setIsStatisticsSelected(false);
+    setIsFaqSelected(false);
+  }
+
+  function goToMyProfile() {
+    setIsSkheratTodoSelected(false);
+    setIsMyProfileSelected(true);
+    setIsStatisticsSelected(false);
+    setIsFaqSelected(false);
+  }
+
+  function goToFaq() {
+    setIsSkheratTodoSelected(false);
+    setIsMyProfileSelected(false);
+    setIsStatisticsSelected(false);
+    setIsFaqSelected(true);
+  }
+
+  function goToStatistics() {
+    setIsSkheratTodoSelected(false);
+    setIsMyProfileSelected(false);
+    setIsStatisticsSelected(true);
+    setIsFaqSelected(false);
+  }
 
   return (
     <div className="drawer-container">
@@ -15,19 +47,52 @@ export const DrawerLayout = props => {
       </div>
       <div className="drawer-menu-items">
         <ul className="menu-items-list">
-          <li className="drawer-menu-item drawer-menu-item-selected">
+          <li
+            className={
+              "drawer-menu-item " +
+              (isSkheratTodoSelected ? "drawer-menu-item-selected" : "")
+            }
+            onClick={goToSkherasTodo}
+          >
             Skheras TODO
           </li>
-          <li className="drawer-menu-item">My Profile</li>
-          <li className="drawer-menu-item">Statistics</li>
-          <li className="drawer-menu-item">FAQ</li>
+          <li
+            className={
+              "drawer-menu-item " +
+              (isMyProfileSelected ? "drawer-menu-item-selected" : "")
+            }
+            onClick={goToMyProfile}
+          >
+            My Profile
+          </li>
+          <li
+            className={
+              "drawer-menu-item " +
+              (isStatisticsSelected ? "drawer-menu-item-selected" : "")
+            }
+            onClick={goToStatistics}
+          >
+            Statistics
+          </li>
+          <li
+            className={
+              "drawer-menu-item " +
+              (isFaqSelected ? "drawer-menu-item-selected" : "")
+            }
+            onClick={goToFaq}
+          >
+            FAQ
+          </li>
         </ul>
       </div>
       <div className="availability-container">
         <div className="availability-label">Availability</div>
-        <ToggleButton value={availability} onToggle={value => {
-            setAvailability(!value)
-        }} />
+        <ToggleButton
+          value={availability}
+          onToggle={value => {
+            setAvailability(!value);
+          }}
+        />
       </div>
     </div>
   );
