848142131d6b44afe28d23fb82861e23b0407f59
Mohammed Aouf ZOUAG <m.a.zouag@gmail.com>
2019-10-08 15:01:46 +0100

    Create the ItemPriceDialog component

---
+++ src/components/rider/skheraDetails/ItemPriceDialog.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+const FormDialog = ({ open, handleClose }) => {
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please specify the price of this item:
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="price"
+            label="Item price"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default FormDialog;
